3f0066a7735090676ed1f02da38b4576248bedb3
Zher Huei Lee <lee.zh.92@gmail.com>
2023-12-11 21:57:41 +0800

    Day 9 (late) commit

---
+++ day09/part1.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+from itertools import pairwise
+
+
+def predict(sequence: list[int]) -> int:
+    difference = [y - x for (x, y) in pairwise(sequence)]
+    if all(x == 0 for x in difference):
+        return sequence[0]
+    prediction = sequence[-1] + predict(difference)
+    return prediction
+
+
+def run(path: os.PathLike) -> str:
+    totals: list[int] = []
+    with open(path, "r", encoding="utf-8") as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line:
+                continue
+            sequence = [int(x) for x in line.split(" ")]
+            prediction = predict(sequence)
+            print(sequence, prediction)
+            totals.append(prediction)
+    result = sum(totals)
+    print("Sum of predictions:", result)
+    return str(result)
+
+
+if __name__ == "__main__":
+    filename = os.path.join(os.path.dirname(__file__), "input.txt")
+    if len(sys.argv) > 1:
+        filename = sys.argv[-1]
+    run(filename)
